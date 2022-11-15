765e58726e97c861fa8addee8da7ab0df1e022bc
Pawansharma08 <mschinu111@gmail.com>
2022-11-15 15:22:38 +0530

    done project

---
+++ app/src/main/java/com/pawan/healthcareapp/Interface/ITimeSlotLoadListener.java
@@ -0,0 +1,11 @@
+package com.pawan.healthcareapp.Interface;
+
+import com.pawan.healthcareapp.model.TimeSlot;
+
+import java.util.List;
+
+public interface ITimeSlotLoadListener {
+    void onTimeSlotLoadSuccess(List<TimeSlot> timeSlotList);
+    void onTimeSlotLoadFailed(String message);
+    void onTimeSlotLoadEmpty();
+}
---
+++ app/src/main/java/com/pawan/healthcareapp/model/ShowToast.java
@@ -0,0 +1,7 @@
+package com.pawan.healthcareapp.model;
+
+public interface ShowToast {
+
+    public void onShowToast (String message);
+
+}
